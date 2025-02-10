3b21e2b73cc0e1cb4d9a5b1e583de004e35ba7cf
luiz <luizoctaviosouza2008@gmail.com>
2025-02-10 20:26:22 -0300

    AULA 10/02

--- index.php
+++ index.php
@@ -8,6 +8,51 @@
     <link rel="stylesheet" href="css/styles.css" />
 </head>
 <body>
+
+<div class="interface">
+
+    <header class="cabecalho">
+
+        <nav class="menu">
+
+            <div class="log">
+                <img src="imagens/logo.png" alt="LOGO" width="150" />
+            </div>
+
+            <ul>
+                <li>Home</li>
+                <li>Serviços</li>
+                <li>Produtos</li>
+                <li>Portifólio</li>
+                <li>Sobre</li>
+                <li>Contato</li>
+            </ul>
+
+            <ol>
+
+            </ol>
+
+        </nav>
+
+        <div class="title">
+
+            <h1 class="titulo">Entregando serviços de qualidade</h1>
+            <h1 class="subtitulo">Soluçôes em TI</h1>
+
+            <p class="paragrafo">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quibusdam eaque, perspiciatis blanditiis vel tempora qui?</p>
+
+        </div>
+
+        <div class="socialIcons">
+            <a href="#">Twitter</a>
+            <a href="#">Facebook</a>
+            <a href="#">Linkednl</a>
+            <a href="#">Instagram</a>
+        </div>
+
+    </header>
+
+</div>
     
 </body>
 </html>

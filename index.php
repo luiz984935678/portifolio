c93aaa7ce18224febf63da955d2ed9129a0b478f
luiz <luizoctaviosouza2008@gmail.com>
2025-02-17 20:28:23 -0300

    AULA 04-02

--- index.php
+++ index.php
@@ -3,44 +3,69 @@
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>portifólio</title>
+    <title>Portifólio</title>
 
     <style>
+        :root {
+            font-size: 16px;
+        }
+
         * {
             margin: 0;
             padding: 0;
-
+            box-sizing: border-box;
         }
 
         body {
-            background-image: url('imagens/logo.png');
+            /* background-image: url('imagens/logo.png');
             background-repeat: no-repeat;
             background-position: center;
             background-size: 100%;
-            background-attachment: fixed;
-            background-
+            background-attachment: fixed; */
+
+            border: 10px solid red;
+            width: 100vw;
+            height: 100vh;
+            overflow-x: hidden;
         }
 
-        p{
-            border: 1px solid red;
+        p {
+            /* border: 1px solid red; */
+            border-width: 10px;
+            border-style: solid;
+            border-color: blue;
+            font-size: 3rem;
         }
 
         h1 {
-            background: red;
+            background-color: yellow;
+            display: block;
+        }
+
+        h1:hover {
+            background-color: red;
+        }
+
+        .logo {
+            position: fixed;
+            bottom: calc(100vh/2);
+            right: 0;
+        }
+
+        #interface {
+
         }
     </style>
-
-    <link rel="stylesheet" href="css/styles.css" />
 </head>
 <body>
 
-<div class="interface">
+<div id="interface">
 
     <header class="cabecalho">
 
         <nav class="menu">
 
-            <div class="log">
+            <div class="logo">
                 <img src="imagens/logo.png" alt="LOGO" width="150" />
             </div>
 
@@ -53,29 +78,43 @@
                 <li>Contato</li>
             </ul>
 
-            <ol>
-
-            </ol>
-
         </nav>
 
-        <div class="title">
+        <div class="titles">
 
             <h1 class="titulo">Entregando serviços de qualidade</h1>
-            <h1 class="subtitulo">Soluçôes em TI</h1>
+            <h1 class="subtitulo">Soluções em TI</h1>
 
-            <p class="paragrafo">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quibusdam eaque, perspiciatis blanditiis vel tempora qui?</p>
+            <p class="paragrafo">Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam quibusdam ab in! A, nam voluptas!</p>
 
         </div>
 
         <div class="socialIcons">
             <a href="#">Twitter</a>
             <a href="#">Facebook</a>
-            <a href="#">Linkednl</a>
+            <a href="#">LinkedIn</a>
             <a href="#">Instagram</a>
         </div>
 
     </header>
+
+    <hr>
+    <br><br><br><br><br><br><br>
+    <hr>
+    <br><br><br><br><br><br><br>
+    <hr>
+    <br><br><br><br><br><br><br>
+    <hr>
+    <br><br><br><br><br><br><br>
+    <hr>
+    <br><br><br><br><br><br><br>
+    <hr>
+    <br><br><br><br><br><br><br>
+    <hr>
+    <br><br><br><br><br><br><br>
+    <hr>
+    <br><br><br><br><br><br><br>
+    <hr>
 
 </div>
     

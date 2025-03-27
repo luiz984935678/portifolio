df6230f4590ec9444d5a5ba149c00bab67a63ecd
luiz <luizoctaviosouza2008@gmail.com>
2025-03-27 20:26:49 -0300

    AULA 27/03

--- index.php
+++ index.php
@@ -10,132 +10,247 @@
 </head>
 <body>
 
-    <div id="interface">
+<div id="interface">
 
-        <header class="cabecalho">
+    <header class="cabecalho">
 
-            <nav class="menu">
+        <nav class="menu">
 
-                <div class="logo">
-                    <img src="imagens/logo.png" alt="LOGO" />
-                </div>
-
-                <ul>
-                    <li><a href="#">Home</a></li>
-                    <li><a href="#">Serviços</a></li>
-                    <li><a href="#">Portifólio</a></li>
-                    <li><a href="#">Sobre</a></li>
-                    <li><a href="#">Contato</a></li>
-                    <li><a href="#">Restrito</a></li>
-                </ul>
-
-            </nav>
-
-            <div class="titles">
-                <div class="container-titles">
-                    <h1 class="titulo">Entregando Serviços de Qualidade</h1>
-                    <h1 class="subtitulo">Soluções de TI</h1>
-                    <p class="paragrafo">Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus culpa, quis nisi blanditiis maiores aspernatur.</p>
-                </div>
-
-                <div class="socialIcons">
-                    <a href="#" title="Visite nosso Twitter"><i class="fab fa-twitter"></i></a>
-                    <a href="#" title="Visite nosso Facebook"><i class="fab fa-facebook"></i></a>
-                    <a href="#" title="Visite nosso Linkedin"><i class="fab fa-linkedin"></i></a>
-                    <a href="#" title="Visite nosso Instagram"><i class="fab fa-instagram"></i></a>
-                </div>
+            <div class="logo">
+                <img src="imagens/logo.png" alt="LOGO" />
             </div>
 
-        </header>
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Serviços</a></li>
+                <li><a href="#">Portifólio</a></li>
+                <li><a href="#">Sobre</a></li>
+                <li><a href="#">Contato</a></li>
+                <li><a href="#">Restrito</a></li>
+            </ul>
 
-        <main id="corpo">
-            <section class="servicos">
+        </nav>
+        
+        <div class="titles"> <!-- ========================= -->
+            <div class="container-titles">
+                <h1 class="titulo">Entregando Serviços de Qualidade</h1>
+                <h1 class="subtitulo">Soluções em TI</h1>
 
-                <article class="servico">
-                    <img src="imagens/service-design.svg" alt="Desenvolvimento WEB">
-                    <h3 class="title-servico">Desenvolvimento WEB</h3>
-                    <p class="text-servico">Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis deleniti doloribus deserunt nam quisquam voluptatibus.</p>
-                </article>
-                <article class="servico">
-                    <img src="imagens/service-hosting.svg" alt="WEB Hosting">
-                    <h3 class="title-servico">Web Hosting</h3>
-                    <p class="text-servico">Explicabo, soluta ad laudantium nihil tempore officiis doloribus. Quas culpa eaque assumenda maxime dolorum adipisci.</p>
-                </article>
-                <article class="servico">
-                    <img src="imagens/service-social.svg" alt="Midias Sociais">
-                    <h3 class="title-servico">Design</h3>
-                    <p class="text-servico">Odit nostrum aperiam, vitae veniam architecto ab quas dignissimos, provident saepe rerum omnis, molestiae corporis!</p>
-                </article>
-                <article class="servico">
-                    <img src="imagens/service-seo.svg" alt="Aplicações SEO">
-                    <h3 class="title-servico">Aplicações SEO</h3>
-                    <p class="text-servico">Labore, alias. Necessitatibus ea officiis quidem, laboriosam delectus, corrupti quia consectetur modi, molestias amet doloremque.</p>
-                </article>
-                <article class="servico">
-                    <img src="imagens/service-cloud.svg" alt="Computação em Nuvem">
-                    <h3 class="title-servico">Computação em Nuvem</h3>
-                    <p class="text-servico">Nostrum tenetur corporis nobis laudantium laborum quo eius ullam excepturi labore nulla! Modi, itaque numquam?</p>
-                </article>
-                <article class="servico">
-                    <img src="imagens/service-secure.svg" alt="Segurança de Dados">
-                    <h3 class="title-servico">Segurança de Dados</h3>
-                    <p class="text-servico">Fugiat sequi ducimus consequuntur, necessitatibus voluptatum voluptate excepturi doloribus ullam harum non aperiam recusandae consequatur.</p>
-                </article>
-
-            </section>
-
-            <section class="portifolio">
-
-                <h3 class="title-portifolio">Nosso Portifólio</h3>
-                <p class="sub-title">Lorem ipsum dolor sit amet.</p>
-
-                <div class="container-imagens">
-                    <a href="#" class="img" style="background-image: url('imagens/product-1.jpg');">
-
-                    </a>
-                    <a href="#" class="img" style="background-image: url('imagens/product-2.jpg');">
-
-                    </a>
-                    <a href="#" class="img" style="background-image: url('imagens/product-3.jpg');">
-
-                    </a>
-                    <a href="#" class="img" style="background-image: url('imagens/product-4.jpg');">
-
-                    </a>
-                    <a href="#" class="img" style="background-image: url('imagens/product-5.jpg');">
-
-                    </a>
-                    <a href="#" class="img" style="background-image: url('imagens/product-6.jpg');">
-
-                    </a>
-                </div>
-
-            </section>
-
-            <section class="banner">
-
-                <hgroup class="container-texto">
-
-                    <h4 class="title-banner">Saiba mais sobre nossos Projetos</h4>
-
-                    <p class="txt-banner">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod suscipit, accusamus voluptatem nostrum rerum aliquid fugit ab temporibus vero, corporis autem, dicta dolor! Deleniti, repellendus!</p>
-
-                    <button class="btn-banner">Saiba mais</button>
-
-                </hgroup>
-
-
-
-            </section>
-
-            <section class="sobre">
-
+                <p class="paragrafo">Lorem ipsum dolor sit amet consectetur adipisicing elit. Id vitae rerum, necessitatibus quod quaerat mollitia.</p>
                 
+            </div>
 
-            </section>
-        </main>
+            <div class="socialIcons">
+                <a href="#" title="Visite nosso Twitter"><i class="fab fa-twitter"></i></a>
+                <a href="#" title="Visite nosso Facebook"><i class="fab fa-facebook"></i></a>
+                <a href="#" title="Visite nosso LinkedIn"><i class="fab fa-linkedin"></i></a>
+                <a href="#" title="Visite nosso Instagram"><i class="fab fa-instagram"></i></a>
+            </div>
+        </div> <!-- ========================== -->
+        
+    </header>
 
-    </div>
-    
+    <!-- ========================================================= -->
+
+    <main id="corpo">
+
+        <section class="servicos">
+
+            <article class="servico">
+                <img src="imagens/service-design.svg" alt="Design">
+                <h3 class="title-servico">Desenvolvimento WEB</h3>
+                <p class="text-servico">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis omnis, voluptas error impedit consectetur vitae.</p>
+            </article>
+            <article class="servico">
+                <img src="imagens/service-hosting.svg" alt="Hosting">
+                <h3 class="title-hosting">WEB Hosting</h3>
+                <p class="text-servico">Possimus vel temporibus deleniti repudiandae in similique unde necessitatibus odit corporis nihil soluta, ad repellendus?</p>
+            </article>
+            <article class="servico">
+                <img src="imagens/service-social.svg" alt="Social Media">
+                <h3 class="title-servico">Mídias Sociais</h3>
+                <p class="text-servico">Earum mollitia a, quaerat magni dignissimos, sint velit libero minus magnam saepe perferendis aspernatur? Libero!</p>
+            </article>
+            <article class="servico">
+                <img src="imagens/service-seo.svg" alt="SEO">
+                <h3 class="title-servico">Aplicações SEO</h3>
+                <p class="text-servico">Maiores aliquid sequi expedita. Rerum itaque nihil maxime at explicabo similique recusandae saepe magnam a!</p>
+            </article>
+            <article class="servico">
+                <img src="imagens/service-cloud.svg" alt="Cloud">
+                <h3 class="title-servico">Computação em Nuvem</h3>
+                <p class="text-servico">Provident velit fuga consequuntur quae, iste accusamus repellendus, officiis quibusdam, odio doloribus voluptates iusto perspiciatis.</p>
+            </article>
+            <article class="servico">
+                <img src="imagens/service-secure.svg" alt="Security">
+                <h3 class="title-servico">Segurança de Dados</h3>
+                <p class="text-servico">Expedita illo fugit error in quidem qui, architecto eligendi quae, perspiciatis modi non ab aliquid.</p>
+            </article>
+
+        </section>
+
+        <section class="portifolio">
+
+            <h3 class="title-portifolio">Nosso Portifólio</h3>
+            <p class="sub-title">Lorem ipsum dolor sit amet.</p>
+
+            <div class="container-imagens">
+                <a href="#" class="img" style="background-image: url('imagens/product-1.jpg');">
+                    
+                </a>
+                <a href="#" class="img" style="background-image: url('imagens/product-2.jpg');">
+                    
+                </a>
+                <a href="#" class="img" style="background-image: url('imagens/product-3.jpg');">
+                    
+                </a>
+                <a href="#" class="img" style="background-image: url('imagens/product-4.jpg');">
+                    
+                </a>
+                <a href="#" class="img" style="background-image: url('imagens/product-5.jpg');">
+
+                </a>
+                <a href="#" class="img" style="background-image: url('imagens/product-6.jpg');">
+                    
+                </a>
+            </div>
+
+        </section>
+
+        <section class="banner">
+
+            <hgroup class="container-texto">
+
+                <h4 class="title-banner">Saiba mais sobre nossos Projetos</h4>
+
+                <p class="txt-banner">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat similique, placeat maxime odio voluptas ducimus pariatur libero accusamus, ad aperiam quis et repellendus cupiditate nostrum.</p>
+
+                <a href="#" class="btn-banner">Saiba mais</a>
+
+            </hgroup>
+
+        </section>
+
+        <section class="sobre">
+
+
+
+        </section>
+
+        <section class="contato">
+
+            <h3 class="title-contato">Fale conosco</h3>
+            <p class="sub-title">Lorem ipsum dolor sit amet consectetur adipisicing.</p>
+
+            <div class="container-contato">
+
+                <aside class="dados-contato">
+                    <h4 class="title-aside">Endereço</h4>
+                    <p class="txt-aside">Rua das Margaridas, S/Nº<br />Centro - Manhuaçu/MG</p>
+
+                    <h4 class="title-aside">Telefone</h4>
+                    <p class="txt-aside">33 3331-3333</p>
+                    <p class="txt-aside">33 9 8400-9999</p>
+
+                    <h4 class="title-aside">E-Mail</h4>
+                    <p class="txt-aside">contato@meusite.com.br</p>
+                    <p class="txt-aside">faleconosco@meusite.com.br</p>
+                </aside>
+
+                <form action="" method="POST">
+
+                    <div class="line-form">
+                        <input type="text" name="nome" id="nome" placeholder="Nome*" required />
+                        <input type="email" name="email" id="email" placeholder="E-Mail*" required />
+                    </div>
+
+                    <div class="line-form">
+                        <input type="text" name="assunto" id="assunto" placeholder="Assunto" />
+                    </div>
+
+                    <div class="line-form">
+                        <textarea name="mensagem" id="mensagem" rows="7" placeholder="Escreva Sua Mensagem*" required></textarea>
+                    </div>
+
+                    <button type="submit">Enviar mensagem</button>
+
+                </form>
+
+            </div>
+
+        </section>
+
+    </main>
+
+    <!-- ========================================================= -->
+
+    <footer id="rodape">
+
+        <div class="sombreamento">
+
+            <div class="container-rodape">
+
+                <section class="item-rodape sobre">
+
+                    <img src="imagens/logo.png" alt="Logo" />
+
+                    <p class="txt-sobre">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Adipisci reiciendis itaque nihil magni excepturi, assumenda in dolore iure necessitatibus deserunt.</p>
+
+                    <h4 class="title-sobre">Siga-nos</h4>
+
+                    <div class="social-icons">
+                        <a href="#" title="Visite nosso Twitter"><i class="fab fa-twitter"></i></a>
+                        <a href="#" title="Visite nosso Facebook"><i class="fab fa-facebook"></i></a>
+                        <a href="#" title="Visite nosso LinkedIn"><i class="fab fa-linkedin"></i></a>
+                        <a href="#" title="Visite nosso Instagram"><i class="fab fa-instagram"></i></a>
+                    </div>
+
+                </section>
+
+                <section class="item-rodape servicos">
+
+                    <h4 class="title-servicos">Serviços</h4>
+
+                    <ul>
+                        <li><a href="#">Desenvolvimento WEB</a></li>
+                        <li><a href="#">WEB Hosting</a></li>
+                        <li><a href="#">Mídias Sociais</a></li>
+                        <li><a href="#">Aplicações SEO</a></li>
+                        <li><a href="#">Computação em Nuvem</a></li>
+                        <li><a href="#">Segurança de Dados</a></li>
+                    </ul>
+
+                </section>
+
+                <section class="item-rodape contato">
+
+                    <h4 class="title-contato">Contatos</h4>
+
+                    <div class="container-contato">
+                        <p class="txt-contatos">Rua das Margaridas, S/Nº</p>
+                        <p class="txt-contatos">33 9 8400-9999</p>
+                        <p class="txt-contatos">contato@meusite.com.br</p>
+                    </div>
+
+                    <h4 class="title-contato">Newsletter</h4>
+                    <form action="" method="GET">
+                        <input type="email" name="news-email" id="news-email" placeholder="E-Mail" />
+
+                        <button type="submit"><i class="fab fa-telegram"></i></button>
+                    </form>
+
+                </section>
+
+            </div>
+
+
+            <p class="txt-creditos">Luiz Octavio Izidoro de Souza &copy; 2025</p>
+
+</div> 
+
+</footer>
+
+</div>
+
 </body>
 </html>
